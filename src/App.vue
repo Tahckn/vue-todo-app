e4e90bfc81923174e384c1f9bd3f1e13830279e2
Taha CEKEN <taha-ceken@hotmail.com>
2022-06-09 19:21:53 +0300

    added styling

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,72 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed, watch } from 'vue';
+
+const todos = ref([]);
+const name = ref('');
+
+const input_content = ref('');
+const input_category = ref(null);
+
+const addTodo = () => {};
+
+const todos_asc = computed(() =>
+  todos.value.sort((a, b) => {
+    return b.createdAt > a.createdAt;
+  })
+);
+
+watch(name, (newVal) => {
+  localStorage.setItem('name', newVal);
+});
+
+onMounted(() => {
+  name.value = localStorage.getItem('name') || '';
+});
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        What's up, <input v-model="name" type="text" placeholder="Name here" />
+      </h2>
+    </section>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+    <section class="create-todo">
+      <h3>CREATE A TODO</h3>
+      <form @submit.prevent="addTodo">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          placeholder="e.g make a video"
+          v-model="input_content"
+        />
+
+        <h4>Pick a category</h4>
+
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="work"
+              v-model="input_category"
+            />
+            <span class="bubble work"></span>
+            <div>work</div>
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>personal</div>
+          </label>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import css from './main.css';
+
 
 createApp(App).mount('#app')
